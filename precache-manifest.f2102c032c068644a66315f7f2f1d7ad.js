f9d37a24cd05b669740fee2a9c5f5d49f44f9f3f
Vishesh Choudhary <visheshc14@gmail.com>
2022-11-09 13:12:01 +0530

    Updates

---
+++ precache-manifest.f2102c032c068644a66315f7f2f1d7ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffcf5006c2d545b184c6d0d94f953685",
+    "url": "/Mareux/index.html"
+  },
+  {
+    "revision": "ac1f5b38ba1826372488",
+    "url": "/Mareux/static/css/main.0b37af9d.chunk.css"
+  },
+  {
+    "revision": "6c413461592f6e83d105",
+    "url": "/Mareux/static/js/2.3e5fcf7e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Mareux/static/js/2.3e5fcf7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac1f5b38ba1826372488",
+    "url": "/Mareux/static/js/main.c9842fd7.chunk.js"
+  },
+  {
+    "revision": "72e3e5211c43f76875a7",
+    "url": "/Mareux/static/js/runtime-main.a89aa580.js"
+  }
+]);
